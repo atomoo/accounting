1dbdbf96030a2ffa86f20c1bbde8a7ff4c383bd0
atomoo <yangpein@gmail.com>
2019-03-29 15:19:46 +0800

    add Model Base

--- store/Model.ts
+++ store/Model.ts
@@ -1,26 +1,62 @@
+import {pickBy} from 'lodash';
 import {AsyncStorage} from 'react-native';
 
-const namespaces: string[] = [];
+// 用来记录保存到AsyncStorage里面的对象
+interface ICollection {
+    // key是每个要保存的对象contructor name 对应保存到storage 的key，value 是不需要保存的字段数组
+    [key: string]: {
+        storageKey: string,
+        ignoreKeys: string[],
+    };
+}
+const collection: ICollection = {};
+
+function findCollectionByStorageKey(storageKey: string) {
+    return Object.keys(collection).find((key: string) => collection[key].storageKey === storageKey);
+}
 
 export function Entity(namespace?: string) {
-    return function(target: any) {
-        const namespaceKey = namespace || target.constructor.name;
-        if (namespaces.indexOf(namespaceKey) === -1) {
-            namespaces.push(namespaceKey);
+    return (target: any) => {
+        const className = target.constructor.name;
+        const storageKey: string = namespace || target.constructor.name;
+        if (collection[className]) {
+            throw new Error(`Duplicate model \`${className}\``);
+        }
+        else if (findCollectionByStorageKey(storageKey)) {
+            throw new Error(`Duplicate storageKey \`${storageKey}\``);
         }
         else {
-            throw new Error(`Duplicate model \`${namespaceKey}\``);
+            collection[className] = {
+                ignoreKeys: [],
+                storageKey,
+            };
         }
     };
 }
 
-export class Model {
-    create() {
-        
+export function Ignore(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    const className = target.constructor.name;
+    const targetIgnoreKeys = collection[className];
+    if (Array.isArray(targetIgnoreKeys) && targetIgnoreKeys.indexOf(propertyKey) === -1) {
+        collection[className].ignoreKeys.push(propertyKey);
     }
-    update() {}
-    search() {}
-    delete() {}
+}
+
+export class Model {
+    public async save() {
+        const className = this.constructor.name;
+        const collectionInfo = collection[className];
+        if (collectionInfo) {
+            const saveObj = pickBy(this, (value, key) => !collectionInfo.ignoreKeys.includes(key));
+            await AsyncStorage.setItem(collectionInfo.storageKey, JSON.stringify(saveObj));
+        }
+        else {
+            throw new Error(`Need to register model`);
+        }
+    }
+    public update() {}
+    public search() {}
+    public delete() {}
 }
 
 // export function
