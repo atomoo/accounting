f390cc53186948a1c2d1de37b2c946e2823940d6
atomoo <yangpein@gmail.com>
2019-06-04 19:03:59 +0800

    add datepicker

--- screen/RecordForm.tsx
+++ screen/RecordForm.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
-import {Picker, View} from 'react-native';
+import {Picker, View, Alert} from 'react-native';
 import {Button, Icon, Input, Text} from 'react-native-elements';
+import DateTimePicker from 'react-native-modal-datetime-picker';
 import ConsumptionType from '../store/ConsumptionType';
 
 interface IPickerOption {
@@ -12,6 +13,10 @@ const RecordForm: React.FC = () => {
     const [amount, setAmount] = useState();
     const [amountError, setAmountError] = useState('');
     const [pickerOptions, setPickerOptions] = useState<IPickerOption[]>([]);
+    const [consumptionType, setConsumptionType] = useState();
+    const [desc, setDesc] = useState('');
+    const [datePickerVisible, setDatePickerVisible] = useState(false);
+    const [createTime, setCreateTime] = useState(new Date());
 
     const handleAmountChange = (val: string) => {
         setAmount(val);
@@ -23,6 +28,28 @@ const RecordForm: React.FC = () => {
         }
     };
 
+    const handleTypeChange = (itemValue: string) => {
+        setConsumptionType(itemValue);
+    };
+
+    const handleDescChange = (descInput: string) => {
+        setDesc(descInput);
+    };
+
+    const handleSubmit = () => {};
+
+    const showDateTimePicker = () => {
+        setDatePickerVisible(true);
+    };
+
+    const hideDateTimePicker = (date: Date) => {
+        setDatePickerVisible(false);
+    };
+
+    const dateTimeChange = (date: Date) => {
+        setCreateTime(date);
+    };
+
     useEffect(() => {
         async function getAllType() {
             const types = await ConsumptionType.find<ConsumptionType>();
@@ -32,6 +59,9 @@ const RecordForm: React.FC = () => {
                     value: t.name,
                 }));
                 setPickerOptions(pickerOptionsFromStore);
+                if (pickerOptionsFromStore.length > 0) {
+                    setConsumptionType(pickerOptionsFromStore[0].value);
+                }
             }
         }
         getAllType();
@@ -46,13 +76,17 @@ const RecordForm: React.FC = () => {
             onChangeText={handleAmountChange}
             errorMessage={amountError}
             value={amount}
+            spellCheck={false}
         />
-        <Text style={{fontSize: 16, color: '#86939E', fontWeight: 'bold', margin: 10, marginBottom: 10}}>
+        <Text style={{fontSize: 16, color: '#86939E', fontWeight: 'bold', margin: 10}}>
             分类
         </Text>
-        <View style={{flexDirection: 'row', alignItems: 'center', margin: 10, marginTop: 0, marginBottom: 10}}>
+        <View style={{flexDirection: 'row', alignItems: 'center', margin: 10, marginTop: 0}}>
             <View style={{flex: 1, borderWidth: 1, borderColor: '#eee'}}>
-                <Picker style={{height: 40}} mode="dropdown">
+                <Picker style={{height: 40}} mode="dropdown"
+                    selectedValue={consumptionType}
+                    onValueChange={handleTypeChange}
+                >
                     {
                         pickerOptions.map((o) => <Picker.Item key={o.value} label={o.label} value={o.value} />)
                     }
@@ -60,13 +94,29 @@ const RecordForm: React.FC = () => {
             </View>
             <Button title="添加分类" type="clear" containerStyle={{width: 100}} />
         </View>
+        <Text style={{fontSize: 16, color: '#86939E', fontWeight: 'bold', margin: 10}}>
+            创建时间
+        </Text>
+        <View style={{flexDirection: 'row', alignItems: 'center', margin: 10, marginTop: 0}}>
+            <Text>{createTime.toString()}</Text>
+            <Button containerStyle={{width: 100}} title="选择时间" type="clear" onPress={showDateTimePicker} />
+        </View>
+        <DateTimePicker mode="datetime"
+            isVisible={datePickerVisible}
+            onCancel={hideDateTimePicker}
+            onConfirm={dateTimeChange}
+        />
         <Input placeholder="描述信息"
             label="备注"
             multiline
             textContentType="none"
+            value={desc}
+            spellCheck={false}
+            onChangeText={handleDescChange}
         />
         <Button title="提交"
             containerStyle={{marginLeft: 10, marginRight: 10, justifyContent: 'flex-end', flex: 1, marginBottom: 30}}
+            onPress={handleSubmit}
         />
     </View>;
 };
